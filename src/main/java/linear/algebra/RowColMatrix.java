87777649168dfa5273527cc0f00a10681b49de9e
rwoo <wooheaven@naver.com>
2016-12-03 12:49:56 +0900

    refactoring previous code

---
+++ src/main/java/linear/algebra/RowColMatrix.java
@@ -0,0 +1,64 @@
+package linear.algebra;
+
+public class RowColMatrix {
+    public double[][] generateRowColMatrtix(int rowSize, int colSise) throws MatrixSizeThrowable {
+        if (rowSize <= 0) {
+            throw new MatrixSizeThrowable("RowColMatrix size should be more than 0. And the current size is " + rowSize);
+        }
+        if (colSise <= 0) {
+            throw new MatrixSizeThrowable("RowColMatrix size should be more than 0. And the current size is " + colSise);
+        }
+
+        double[][] resultRowColMatrix = new double[rowSize][colSise];
+
+        for (int i = 0; i < resultRowColMatrix.length; i++) {
+            for (int j = 0; j < resultRowColMatrix[0].length; j++) {
+                resultRowColMatrix[i][j] = Math.random();
+            }
+        }
+
+        return resultRowColMatrix;
+    }
+
+    public double[][] ijkRowColMatrtixMultiplication(double[][] aRowColMatrix, double[][] bRowColMatrix) throws MatrixSizeThrowable {
+        if (aRowColMatrix[0].length != bRowColMatrix.length) {
+            throw new MatrixSizeThrowable("aMat colmunSize should be same bMat rowSize.\n"
+                    + "aMat columnSize = " + aRowColMatrix[0].length + "\n"
+                    + "bMat rowSize = " + bRowColMatrix.length);
+        }
+        int n = aRowColMatrix[0].length;
+
+        double[][] resultRowColMatrix = new double[aRowColMatrix.length][bRowColMatrix[0].length];
+
+        for (int i = 0; i < aRowColMatrix.length; i++) {
+            for (int j = 0; j < bRowColMatrix[0].length; j++) {
+                for (int k = 0; k < n; k++) {
+                    resultRowColMatrix[i][j] += aRowColMatrix[i][k] * bRowColMatrix[k][j];
+                }
+            }
+        }
+
+        return resultRowColMatrix;
+    }
+
+    public double[][] ikjRowColMatrtixMultiplication(double[][] aRowColMatrix, double[][] bRowColMatrix) throws MatrixSizeThrowable {
+        if (aRowColMatrix[0].length != bRowColMatrix.length) {
+            throw new MatrixSizeThrowable("aMat colmunSize should be same bMat rowSize.\n"
+                    + "aMat columnSize = " + aRowColMatrix[0].length + "\n"
+                    + "bMat rowSize = " + bRowColMatrix.length);
+        }
+        int n = aRowColMatrix[0].length;
+
+        double[][] resultRowColMatrix = new double[aRowColMatrix.length][bRowColMatrix[0].length];
+
+        for (int i = 0; i < aRowColMatrix.length; i++) {
+            for (int k = 0; k < n; k++) {
+                for (int j = 0; j < bRowColMatrix[0].length; j++) {
+                    resultRowColMatrix[i][j] += aRowColMatrix[i][k] * bRowColMatrix[k][j];
+                }
+            }
+        }
+
+        return resultRowColMatrix;
+    }
+}
---
+++ src/test/java/linear/algebra/RowColMatrixTest.java
@@ -0,0 +1,50 @@
+package linear.algebra;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.util.StopWatch;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class RowColMatrixTest {
+    private StopWatch st;
+    private RowColMatrix rowColMatrix;
+    private double[][] aRowColMatrix;
+    private double[][] bRowColMatrix;
+    private double[][] cRowColMatrix;
+    private double[][] dRowColMatrix;
+    private double cMatValue;
+    private double dMatValue;
+    private double different;
+
+    @Before
+    public void setUp() throws MatrixSizeThrowable {
+        st = new StopWatch("RowColMatrix Multiplication");
+        rowColMatrix = new RowColMatrix();
+        aRowColMatrix = rowColMatrix.generateRowColMatrtix(300, 200);
+        bRowColMatrix = rowColMatrix.generateRowColMatrtix(200, 300);
+    }
+
+    @Test
+    public void rowColMatrtixMul() throws MatrixSizeThrowable {
+        st.start("ijk RowColMatrix Multiplication");
+        cRowColMatrix = rowColMatrix.ijkRowColMatrtixMultiplication(aRowColMatrix, bRowColMatrix);
+        st.stop();
+
+        st.start("ikj RowColMatrix Multiplication");
+        dRowColMatrix = rowColMatrix.ikjRowColMatrtixMultiplication(aRowColMatrix, bRowColMatrix);
+        st.stop();
+
+        for (int i = 0; i < cRowColMatrix.length; i++) {
+            for (int j = 0; j < cRowColMatrix[i].length; j++) {
+                cMatValue = cRowColMatrix[i][j];
+                dMatValue = dRowColMatrix[i][j];
+                different = Math.abs(cMatValue - dMatValue);
+                assertThat("Compare RowColMatrix["+i+"]["+j+"] is different", different, is(0.0));
+            }
+        }
+
+        System.out.println(st.prettyPrint());
+    }
+}
